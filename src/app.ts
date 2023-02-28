6605b32532cd27eafa9d8de180c1c712cabbe694
Kathy Adams <kaadams1@gmail.com>
2023-02-28 09:06:17 -0700

    Updated file structure

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@
 /// <reference path="state/project-state.ts" />
 /// <reference path="util/validation.ts" />
 /// <reference path="decorators/autobind.ts" />
-/// <reference path="components/project-item.ts" />
 /// <reference path="components/project-list.ts" />
 /// <reference path="components/project-input.ts" />
 
